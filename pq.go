d2f2b1c4e63308f659c9f652a369454a326d7540
Cem Sancak <cem.sancak.90@gmail.com>
2024-05-14 21:09:50 +0300

    Revert "add WriteValue func (#1)"
    This reverts commit 716dbf8a1ba8c70473be9bccee33bdf7ea06a4e7.

--- pq.go
+++ pq.go
@@ -1,14 +1,9 @@
 package priority
 
-import (
-	"sync/atomic"
-)
-
 type PriorityQueue struct {
 	lowest, highest int
 	main            <-chan interface{}
 	ins             map[int]chan<- interface{}
-	closed          atomic.Bool
 }
 
 const (
@@ -58,7 +53,6 @@ func NewPriorityQueue(numQueues int, ascendingPriority, blocking bool) *Priority
 }
 
 func (pq *PriorityQueue) Close() {
-	pq.closed.Store(true)
 	close(pq.ins[pq.lowest])
 }
 
@@ -74,14 +68,6 @@ func (pq *PriorityQueue) Write(priority int) chan<- interface{} {
 		in = pq.ins[pq.lowest]
 	}
 	return in
-}
-
-func (pq *PriorityQueue) WriteValue(priority int, msg interface{}) bool {
-	if pq.closed.Load() {
-		return false
-	}
-	pq.Write(priority) <- msg
-	return true
 }
 
 func (pq *PriorityQueue) ReadChan() <-chan interface{} {
